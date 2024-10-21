e133d16134c8e1169cd677f7e7a17ca3be9e2191
johnnysuh <nicolas.c.duarte@estudante.sesisenai.org.br>
2024-10-21 09:45:57 -0300

    oi

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 import Express from 'express'
 
 const app = Express()
+app.use(Express.json())
 
 app.get('/pegar', function (req, res) {
     res.send('hit solo da gaga')
@@ -8,6 +9,25 @@ app.get('/pegar', function (req, res) {
 
 app.get('/pegaroutracoisa', function (req, res) {
     res.send('charli xcx')
+})
+
+app.post('/registro', function(req, res) {
+    // verificar se todos os campos foram enviados
+    try{
+        console.log(req)
+        const dados = req.body
+        if(!nome || !sobrenome || !email || !senha || !dataNascimento){
+            res.send('Todos os campos devem ser enviados')
+            return
+        }
+        console.log('criar user')
+        res.send('ok')
+    } catch (erro) {
+
+    }
+    // encriptar senha do usuario
+    // criar usuario na DB
+    // devolver resposta para meu cliente
 })
 
 app.listen(7000)
